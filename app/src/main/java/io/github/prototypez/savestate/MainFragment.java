72c395940bb8e792cec1cbbb22cea479955d837c
prototypez <prototypez@126.com>
2018-06-16 20:09:59 +0800

    feat(demo): plugin version -> 0.1.2

--- app/src/main/java/io/github/prototypez/savestate/MainFragment.java
+++ app/src/main/java/io/github/prototypez/savestate/MainFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.List;
 import java.util.Map;
@@ -20,7 +23,7 @@ public class MainFragment extends Fragment {
     List<Map<String, Object>> response;
 
 
-//    @Override
+    //    @Override
 //    public void onSaveInstanceState(@NonNull Bundle outState) {
 //        super.onSaveInstanceState(outState);
 //    }
